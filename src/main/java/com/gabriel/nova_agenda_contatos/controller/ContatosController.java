4991d0423eefee3a4fe52cfcbbd4a4fd9ea930bc
Gabriel <gabriel.benini2@gmail.com>
2025-05-27 19:51:50 -0300

    Trocando a anotacao @Controller por @RestController

--- src/main/java/com/gabriel/nova_agenda_contatos/controller/ContatosController.java
+++ src/main/java/com/gabriel/nova_agenda_contatos/controller/ContatosController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/contatos")
 @RequiredArgsConstructor
 public class ContatosController {
