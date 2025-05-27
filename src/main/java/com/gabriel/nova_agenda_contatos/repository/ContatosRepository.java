c656d90b74a3f2928affd0f28eff48b992c0a00e
Gabriel <gabriel.benini2@gmail.com>
2025-05-27 18:16:30 -0300

    Criada a interface repository para os contatos

---
+++ src/main/java/com/gabriel/nova_agenda_contatos/repository/ContatosRepository.java
@@ -0,0 +1,7 @@
+package com.gabriel.nova_agenda_contatos.repository;
+
+import com.gabriel.nova_agenda_contatos.model.Contatos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContatosRepository extends JpaRepository<Contatos, Long> {
+}
